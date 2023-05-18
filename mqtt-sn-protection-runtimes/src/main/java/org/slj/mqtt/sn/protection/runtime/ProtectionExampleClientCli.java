c0c033e64d8f6be186f5b0da8de1aaa4829071b0
Simon Johnson <2924684+simon622@users.noreply.github.com>
2023-05-18 09:08:01 +0100

    Portection Example Runtimes

---
+++ mqtt-sn-protection-runtimes/src/main/java/org/slj/mqtt/sn/protection/runtime/ProtectionExampleClientCli.java
@@ -0,0 +1,29 @@
+package org.slj.mqtt.sn.protection.runtime;
+
+import org.slj.mqtt.sn.client.impl.MqttsnClientRuntimeRegistry;
+import org.slj.mqtt.sn.client.impl.cli.MqttsnInteractiveClient;
+import org.slj.mqtt.sn.client.impl.cli.MqttsnInteractiveClientLauncher;
+import org.slj.mqtt.sn.codec.MqttsnCodecs;
+import org.slj.mqtt.sn.impl.AbstractMqttsnRuntimeRegistry;
+import org.slj.mqtt.sn.integrity.MqttsnProtectionService;
+import org.slj.mqtt.sn.model.MqttsnOptions;
+import org.slj.mqtt.sn.spi.IMqttsnStorageService;
+import org.slj.mqtt.sn.spi.IMqttsnTransport;
+
+/**
+ * @author Simon L Johnson
+ */
+public class ProtectionExampleClientCli {
+    public static void main(String[] args) throws Exception {
+        MqttsnInteractiveClientLauncher.launch(new MqttsnInteractiveClient() {
+            protected AbstractMqttsnRuntimeRegistry createRuntimeRegistry(IMqttsnStorageService storageService, MqttsnOptions options, IMqttsnTransport transport) {
+                AbstractMqttsnRuntimeRegistry registry = MqttsnClientRuntimeRegistry.defaultConfiguration(storageService, options).
+                        withTransport(transport).
+                        //-- Davide this is the place to bootstrap the instance into the runtime
+                        withSecurityService(new MqttsnProtectionService()).
+                        withCodec(MqttsnCodecs.MQTTSN_CODEC_VERSION_2_0);
+                return registry;
+            }
+        });
+    }
+}
---
+++ mqtt-sn-protection-runtimes/src/main/java/org/slj/mqtt/sn/protection/runtime/ProtectionExampleGatewayCli.java
@@ -0,0 +1,44 @@
+package org.slj.mqtt.sn.protection.runtime;
+
+import org.slj.mqtt.sn.client.impl.MqttsnClientRuntimeRegistry;
+import org.slj.mqtt.sn.client.impl.cli.MqttsnInteractiveClient;
+import org.slj.mqtt.sn.client.impl.cli.MqttsnInteractiveClientLauncher;
+import org.slj.mqtt.sn.codec.MqttsnCodecs;
+import org.slj.mqtt.sn.gateway.cli.MqttsnInteractiveGateway;
+import org.slj.mqtt.sn.gateway.cli.MqttsnInteractiveGatewayLauncher;
+import org.slj.mqtt.sn.gateway.impl.MqttsnGatewayRuntimeRegistry;
+import org.slj.mqtt.sn.gateway.impl.connector.LoopbackMqttsnConnector;
+import org.slj.mqtt.sn.gateway.impl.gateway.type.MqttsnAggregatingGateway;
+import org.slj.mqtt.sn.gateway.spi.connector.MqttsnConnectorOptions;
+import org.slj.mqtt.sn.gateway.spi.gateway.MqttsnGatewayOptions;
+import org.slj.mqtt.sn.gateway.spi.gateway.MqttsnGatewayPerformanceProfile;
+import org.slj.mqtt.sn.impl.AbstractMqttsnRuntimeRegistry;
+import org.slj.mqtt.sn.integrity.MqttsnProtectionService;
+import org.slj.mqtt.sn.model.MqttsnOptions;
+import org.slj.mqtt.sn.spi.IMqttsnStorageService;
+import org.slj.mqtt.sn.spi.IMqttsnTransport;
+
+/**
+ * @author Simon L Johnson
+ */
+public class ProtectionExampleGatewayCli {
+    public static void main(String[] args) throws Exception {
+        MqttsnInteractiveGatewayLauncher.launch(new MqttsnInteractiveGateway() {
+            protected AbstractMqttsnRuntimeRegistry createRuntimeRegistry(IMqttsnStorageService storageService, MqttsnOptions options, IMqttsnTransport transport) {
+
+                MqttsnConnectorOptions connectorOptions = new MqttsnConnectorOptions();
+                IMqttsnStorageService namespacePreferences = storageService.getPreferenceNamespace(LoopbackMqttsnConnector.DESCRIPTOR);
+                namespacePreferences.initializeFieldsFromStorage(connectorOptions);
+
+                return MqttsnGatewayRuntimeRegistry.defaultConfiguration(storageService, (MqttsnGatewayOptions)options).
+                        withConnector(new LoopbackMqttsnConnector(LoopbackMqttsnConnector.DESCRIPTOR, connectorOptions)).
+                        withBackendService(new MqttsnAggregatingGateway()).
+                        //-- Davide this is the place to bootstrap the instance into the runtime
+                        withSecurityService(new MqttsnProtectionService()).
+                        withCodec(MqttsnCodecs.MQTTSN_CODEC_VERSION_2_0).
+                        withTransport(createTransport(storageService));
+
+            }
+        }, false, "Welcome to the loopback gateway. This version does NOT use a backend broker, instead brokering MQTT messages itself as a loopback to connected devices.");
+    }
+}

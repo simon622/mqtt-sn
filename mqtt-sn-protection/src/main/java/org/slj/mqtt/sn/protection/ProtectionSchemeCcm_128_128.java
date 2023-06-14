2ba10f76dcf3e638634ca6bacdd529b4ec510778
lenzarda <davide.lenzarini@ieee.org>
2023-06-14 14:51:34 +0200

    Protection Packet wip: protection packet - protection scheme 0x40 bugs removed, 041, 0x42, 0x43, 0x44, 0x45 completed

---
+++ mqtt-sn-protection/src/main/java/org/slj/mqtt/sn/protection/ProtectionSchemeCcm_128_128.java
@@ -0,0 +1,22 @@
+package org.slj.mqtt.sn.protection;
+
+public class ProtectionSchemeCcm_128_128 extends AbstractProtectionSchemeCcm
+{
+	public static void register()	
+	{
+		protectionSchemeClasses.put(Byte.valueOf(AES_CCM_128_128), ProtectionSchemeCcm_128_128.class);
+	}
+
+	public static void unregister()	
+	{
+		protectionSchemeClasses.remove(Byte.valueOf(AES_CCM_128_128));
+	}
+
+	public ProtectionSchemeCcm_128_128(String name, byte index)
+	{
+		super(name,index);
+		this.nominalTagLengthInBytes=16; //128 bits tag
+		this.nominalTagLengthInBits=(short)(this.nominalTagLengthInBytes*8);
+		this.allowedKeyLength=16; //128 bits keys allowed in this scheme
+	}
+}
